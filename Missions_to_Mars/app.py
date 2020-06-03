3849fe7f4010ca95d80589ca4838cd264378e56f
Bosco Sitati <39392239+bsitati@users.noreply.github.com>
2020-06-03 03:38:41 -0500

    Add files via upload

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,30 +1,39 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_mars
+# import scrape_mars
 
 app = Flask(__name__)  
+# Create an instance of Flask
 
 # Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/scrapemars_app"
-# mongo = PyMongo(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_scrape_app"
+mongo = PyMongo(app)
 
-# Or set inline
-mongo = PyMongo(app, uri="mongodb://localhost:27017/scrapemars_app")
+# Use PyMongo to establish Mongo connection
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_scrape_app")
 
 
+# Route to render index.html template using data from Mongo
 @app.route("/")
-def index():
-    scrape_mars = mongo.db.scrape_mars_db.find_one()
-    return render_template("index.html", scrapings=scrape_mars)
+def home():
+
+    # Find one record of data from the mongo database
+    mars_scrape_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", scrapings=mars_scrape_data)
 
 
+# Route that will trigger the scrape function
 @app.route("/scrape")
-def scraper():
-    # scrape_mars = mongo.db.scrape_mars_db
-    # Run the scrape function
+def scrape():
 
-    scraping_data = scrape_mars.scrape()
-    scrape_mars.update({}, scraping_data, upsert=True)
+   # Run the scrape function
+    scrape_mars_data = scrape_mars.scrape_mars_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, scrape_mars_data, upsert=True)
+
     return redirect("/", code=302)
 
 #-----------------------
--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,64 +1,53 @@
 # Dependencies
+import os
 from splinter import Browser
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import time
+from pprint import pprint
+import cssutils
 
 
-# def init_browser():
-#     # @NOTE: Replace the path with your actual path to the chromedriver
-#     # executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-#     executable_path = {"executable_path": "chromedriver"}
-#     return Browser("chrome", **executable_path, headless=False)
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    # executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    executable_path = {"executable_path": "chromedriver.exe"}
+    return Browser("chrome", **executable_path, headless=False)
 
-def scrape():
+
+def scrape_info():
     browser = init_browser()
     scrape_mars = {}   
     # URL of page to be scraped
-    url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+    time.sleep(5)
+    html = browser.html
 
-    # Retrieve page with the requests module
-    response = requests.get(url)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(html, 'html.parser')
     # results are returned as an iterable list
-    results = soup.find('div', class_="features")
+    results = soup.find_all('li', class_='slide')
     # Find titles
     
-    for result in results.findAll('div', attrs={'class':'content_title'}):
-#     print(div.find('a')['href'])
-        news_title = result.find_all("a")[0].string
-#     print(news_title)
+    news_title = results[0].find('div', class_='content_title').text
+    news_p = results[0].find('div', class_='rollover_description_inner').text
+    # if (news_title and news_p):
+    #     print(news_title)
+    #     print(news_p)
 
-    scrape_mars['titles'] = news_title
+    scrape_mars['news_title'] = news_title
+    scrape_mars['news_p'] = news_p
     #----------------------------------
-    #paragraphs
-    
-    for result in results.find_all('div', class_='rollover_description_inner'):
-        news_p = result.text
-        paragraphs.append(news_p)
-    
-    scrape_mars['paragraphs'] = paragraphs
-
-    #JPL Mars Space Images - Featured Image
-    # URL of page to be scraped
-    url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
-
-    # Retrieve page with the requests module
-    response = requests.get(url)
-    #-----------------------------------
+    #JPL Mars Space Images - Featured Image 
     #
     
-    import cssutils
-
+   
     # browser.visit(url)
     url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     html = requests.get(url)
     # soup = BeautifulSoup(html, 'html.parser')
     soup = BeautifulSoup(html.text, 'html.parser')
-
-    # results = soup.find_all('article', class_='carousel_item').find()
-
-    #featured_image_url = results.find('span', class_='text')
 
     div_style = soup.find('article')['style']
     style = cssutils.parseStyle(div_style)
@@ -75,7 +64,6 @@ def scrape():
     scrape_mars['featured_image_url'] = featured_image_url
     #--------------------------
     #fact table
-    import pandas as pd
     url = "https://space-facts.com/mars/"
     html = requests.get(url)
     # soup = BeautifulSoup(html, 'html.parser')
@@ -85,11 +73,8 @@ def scrape():
 
     facts_df = pd.read_html(url)
         
-    facts_df[0]
-
-    fact_html = facts_df[0].to_html('factMars.html')
-    
-    scrape_mars['fact_html'] = fact_html
+    # fact_html = facts_df[0].to_html('factMars.html')
+    scrape_mars['fact_html'] = facts_df[0]
 
     #--------------------------
     # Mars Hemispheres
@@ -178,4 +163,4 @@ def scrape():
 
     #return scrape_mars
 
-    return scrape_mars
+    return scrape_mars_data
