b9a232130ac6faaa711ade6a74de27b5ae5087fd
Bosco Sitati <39392239+bsitati@users.noreply.github.com>
2020-06-04 15:08:39 -0500

    Add files via upload

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -3,24 +3,25 @@ import os
 from splinter import Browser
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
+# import pandas as pd
 import time
 from pprint import pprint
 import cssutils
+import re
 
 
 def init_browser():
     # @NOTE: Replace the path with your actual path to the chromedriver
-    # executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-    executable_path = {"executable_path": "chromedriver.exe"}
+    executable_path = {"executable_path": "c:/Users/c2c/Documents/GitHub/web-scraping-challenge/Missions_to_Mars/chromedriver.exe"}
+    # executable_path = {"executable_path": "chromedriver.exe"}
     return Browser("chrome", **executable_path, headless=False)
 
 
 def scrape_info():
-    browser = init_browser()
-    scrape_mars = {}   
-    # URL of page to be scraped
+    scrape_mars_data ={}
     url = 'https://mars.nasa.gov/news/'
+    browser = init_browser()
+
     browser.visit(url)
     time.sleep(5)
     html = browser.html
@@ -29,15 +30,15 @@ def scrape_info():
     # results are returned as an iterable list
     results = soup.find_all('li', class_='slide')
     # Find titles
-    
+        
     news_title = results[0].find('div', class_='content_title').text
     news_p = results[0].find('div', class_='rollover_description_inner').text
     # if (news_title and news_p):
     #     print(news_title)
     #     print(news_p)
 
-    scrape_mars['news_title'] = news_title
-    scrape_mars['news_p'] = news_p
+    scrape_mars_data['news_title'] = news_title
+    scrape_mars_data['news_p'] = news_p
     #----------------------------------
     #JPL Mars Space Images - Featured Image 
     #
@@ -55,13 +56,14 @@ def scrape_info():
 
     back_url = url.replace('url(', '').replace(')', '')    # or regex/split/find/slice etc.
 
-    front_url = soup.find('a').get('href')
+    # front_url = soup.find('a').get('href')
+    front_url = "https://www.jpl.nasa.gov"
 
     featured_image_url = front_url + back_url
 
     # featured_image_url.append(featured_image_url)
 
-    scrape_mars['featured_image_url'] = featured_image_url
+    scrape_mars_data['featured_image_url'] = featured_image_url
     #--------------------------
     #fact table
     url = "https://space-facts.com/mars/"
@@ -71,15 +73,13 @@ def scrape_info():
 
     tb = soup.find_all('table', class_='tablepress tablepress-id-p-mars')
 
-    facts_df = pd.read_html(url)
-        
-    # fact_html = facts_df[0].to_html('factMars.html')
-    scrape_mars['fact_html'] = facts_df[0]
+    # facts_df = pd.read_html(url)
+    # output html table
+    # scrape_mars_data['fact_html'] = facts_df[0]
 
-    #--------------------------
+    # --------------------------
     # Mars Hemispheres
 
-    import re
     hemisphere_image_urls =[]
 
     image_url1 = {}
@@ -154,13 +154,12 @@ def scrape_info():
     hemisphere_image_urls.append(image_url4)
     #-------------------------------
 
-    scrape_mars['hemisphere_image_urls'] = hemisphere_image_urls
-    # scrape_mars.append(hemisphere_image_urls)
-
-
-    
+    scrape_mars_data['hemisphere_image_urls'] = hemisphere_image_urls
     #----------------------------------
 
     #return scrape_mars
 
     return scrape_mars_data
+
+
+scrape_info()
